91289498f04221c6b74f6e0ede4f60119a71c277
SAI CHARAN SALUGARI <sunny@Mac.lan>
2025-03-06 13:53:51 -0500

    initial

--- components/header.jsx
+++ components/header.jsx
@@ -23,15 +23,15 @@ export default async function Header() {
   await checkUser();
 
   return (
-    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+    <header className="fixed top-3 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/">
           <Image
             src={"/logo.png"}
             alt="Sensai Logo"
-            width={200}
-            height={60}
-            className="h-12 py-1 w-auto object-contain"
+            width={300}
+            height={90}
+            className="h-25 py-1 w-auto object-contain"
           />
         </Link>
 
